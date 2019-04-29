30835b47fcb47bf6786681f546ca83c5c3eb0880
phanlop.p <phanlop.p@groupaviathailand.com>
2019-04-29 11:29:14 +0700

    insert element to HTML

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -42,11 +42,9 @@ var budgetController = (function() {
             }
             
             // Create new item based on 'inc' or 'exp' type
-            if(type === 'exp') {
-                
+            if(type === 'exp') {               
                 newItem = new Expense(ID, des, val);
-            } else if (type === 'inc') {
-                
+            } else if (type === 'inc') {    
                 newItem = new Income(ID, des, val);
             }
             
@@ -72,7 +70,9 @@ var UIController = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
         
     }
 
@@ -83,6 +83,42 @@ var UIController = (function() {
                 description: document.querySelector(DOMStrings.inputDescription).value,
                 value: document.querySelector(DOMStrings.inputValue).value,
             }
+        },
+
+        addListItem: function(obj, type) {
+            var html, newHtml;
+            // Create HTML string with placeholder text
+
+            if (type === 'inc') {
+                element = DOMStrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%">'
+                +'<div class="item__description">%description%</div>'
+                +'<div class="right clearfix">'
+                +'<div class="item__value">%value</div>'
+                +'<div class="item__delete">'
+                +'<button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>'
+                +'</div></div></div>';
+            } else if (type === 'exp') {
+                element = DOMStrings.expensesContainer;
+                html = '<div class="item clearfix" id="expense-%id%">'
+                +'<div class="item__description">%description%</div>'
+                +'<div class="right clearfix">'
+                +'<div class="item__value">%value%</div>'
+                +'<div class="item__percentage">21%</div>'
+                +'<div class="item__delete">'
+                +'<button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>'
+                +'</div></div></div>';
+            }
+
+            // Replace the placeholder text with some actual data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            // Insert the HTML into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
+
         },
 
         getDOMStrings: function() {
@@ -121,7 +157,10 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 2. Add the item to the budget controller.
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         budgetCtrl.testing();
+
         // 3. add the item to the UI.
+        UICtrl.addListItem(newItem, input.type);
+
         // 4. calculate the budget.
         // 5. display the budget on the UI.
         console.log("It work")
