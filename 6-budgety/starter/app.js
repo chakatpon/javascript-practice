b18d0bca90f0d840ff050662b5289d2a131e2cd1
phanlop.p <phanlop.p@groupaviathailand.com>
2019-04-29 17:22:57 +0700

    add clearFields function

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -81,7 +81,7 @@ var UIController = (function() {
             return {
                 type: document.querySelector(DOMStrings.inputType).value, // Will be either inc or exp.
                 description: document.querySelector(DOMStrings.inputDescription).value,
-                value: document.querySelector(DOMStrings.inputValue).value,
+                value: parseFloat(document.querySelector(DOMStrings.inputValue).value),
             }
         },
 
@@ -94,7 +94,7 @@ var UIController = (function() {
                 html = '<div class="item clearfix" id="income-%id%">'
                 +'<div class="item__description">%description%</div>'
                 +'<div class="right clearfix">'
-                +'<div class="item__value">%value</div>'
+                +'<div class="item__value">%value%</div>'
                 +'<div class="item__delete">'
                 +'<button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>'
                 +'</div></div></div>';
@@ -119,6 +119,20 @@ var UIController = (function() {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
 
 
+        },
+
+        clearFields: function() {
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMStrings.inputDescription + ', ' + DOMStrings.inputValue);
+
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });
+
+            fieldsArr[0].focus();
         },
 
         getDOMStrings: function() {
@@ -146,6 +160,16 @@ var controller = (function(budgetCtrl, UICtrl) {
         });
     }
 
+    var updateBudget = function() {
+
+        // 1. Calculate the budget
+
+        // 2. Return the budget
+
+        // 3. Display the budget on the UI
+
+
+    }
     
     var ctrlAddItem = function() {
         var input, newItem;
@@ -153,17 +177,26 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 1. get the filed input data.
         input = UICtrl.getInput();
         console.log(input);
+
+        if (input.description !== '' && !isNaN(input.value) && input.value > 0 ) {
+            // 2. Add the item to the budget controller.
+           newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+           budgetCtrl.testing();
+
+           // 3. add the item to the UI.
+           UICtrl.addListItem(newItem, input.type);
+
+           // 4. Clear the fields.
+           UICtrl.clearFields();
+
+           // 5. calculate and update the budget.
+           updateBudget();
+
+           // 6. display the budget on the UI.
+           console.log("It work")
+        }
        
-        // 2. Add the item to the budget controller.
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-        budgetCtrl.testing();
 
-        // 3. add the item to the UI.
-        UICtrl.addListItem(newItem, input.type);
-
-        // 4. calculate the budget.
-        // 5. display the budget on the UI.
-        console.log("It work")
     }
 
     return {
